82908f3bbf0001f290b1f49c64448f686c24e8c7
dictvm <dh@horrendum.de>
2013-12-21 10:43:28 +0100

    added AI-/player-selection and randomness for numbers chosen by AI

--- calc.go
+++ calc.go
@@ -2,18 +2,30 @@ package main
 
 import (
     "fmt"
+    "math/rand"
+    "time"
 )
 
 func main() {
 
 // also based on: http://piratepad.net/XJnL7LdAUx
 
-    var inputNum int
-    var guessedNum int
-    var tries int = 0
+    var inputNum, guessedNum, tries, opponentNum int
+    var opponentSelected bool = false
 
-    fmt.Println("Enter a number between 1 and 10 that your opponent shall guess.")
-    inputNum = getInput()
+    for opponentSelected == false {
+        fmt.Println("Choose your opponent: \n 0 = human \n 1 = AI \n")
+        fmt.Scanln(&opponentNum)
+            if opponentNum == 0 {
+                opponentSelected = true
+                fmt.Println("Enter a number between 1 and 10 that your opponent shall guess.")
+                inputNum = getInput()
+            } else if opponentNum == 1 {
+                opponentSelected = true
+                inputNum = random(1, 10)
+                fmt.Println("The AI has chosen a random number for you to guess.")
+            }
+        }
 
     fmt.Println("Now try to guess your opponents' input.")
 
@@ -24,6 +36,7 @@ func main() {
 
         if guessEqualsInput(inputNum, guessedNum) == true {
             fmt.Println("Good job, your guess was correct.")
+            fmt.Println("The entered number was", inputNum, ".")
             break // exits the current for-loop
         }
 
@@ -35,7 +48,7 @@ func main() {
     }
 
     if guessEqualsInput(inputNum, guessedNum) != true {
-        fmt.Println("You lost the game. Your opponent entered ", inputNum, ".")
+        fmt.Println("You lost the game. Your opponent entered", inputNum, ".")
     }
 
 }
@@ -61,4 +74,9 @@ func guessEqualsInput(inputNum int, guessedNum int) bool {
     } else {
     return false
     }
+}
+
+func random(min, max int) int {
+    rand.Seed(time.Now().Unix())
+    return rand.Intn(max - min) + min
 }
